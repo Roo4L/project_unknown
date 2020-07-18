c2791ef9c262816cbdf15c930179432935c02034
Roo4L <kesha.plovec02@gmail.com>
2020-07-18 07:08:38 +0500

    Define widjet data containers

--- data_structures.cpp
+++ data_structures.cpp
@@ -1,33 +1,10 @@
 #include "data_structures.h"
 
 template <typename Content>
-group::group(const FILE* src, size_t offset) {
-	fseek(src, offset, SEEKSET);
-	char name_tmp[80];
-	fread(name_tmp, sizeof(char), 80, src);
-	this->name(name_tmp);
-
-	size_t content_offset, int len;
-	fread(len, sizeof(int), 1, src);
-	fread(content_offset, sizeof(size_t), 1, src);
-	fseek(src, content_offset);
-	arr = (Content *)malloc(sizeof(Content) * len);
-	for (int k = 0; k < n; k++) {
-		arr[k](src, content_offset + sizeof(Content));
-	}
+group::group(const std::ifstream* src, std::streampos offset) {
 }
 
 template<typename Content>
 int group::size() {
 	return sz;
-}
-
-template<typename Content>
-group<Content>* begin(group_table<Content>& x) {
-	return x.size() ? &x[0] : nullptr;
-}
-
-template<typename Content>
-group<Content>* end(group_table<Content>& x) {
-	return x.size ? &x[0]+x.size() : nullptr;
 }
--- data_structures.h
+++ data_structures.h
@@ -1,77 +1,115 @@
- class link
- {
- private:
- 	string name;
- 	string addr;
- public:
- 	link(const string& name, const string& addr):
- 	  name(name)
- 	, addr(addr){};
- 	link(const FILE* f, )
- }
+#include <fstream>
+#include <QString>
+#include <QUrl>
+
+#define SPIRIT_INIT true
+#define SPIRIT_CREATE false
+
+namespace fs = std:filesystem;
+
+/*
+ * Project main class for interaction with all project components.
+ * Initialized with path to project folder and existence flag.
+ * Constucted during openning project.
+ * Destructed during closing.
+ */
+class spirit
+{
+private:
+	fs::path project_folder;
+	std::vector<fs:path> blog_files;
+	bool git_exists;
+	QString git_origin;
+public:
+	spirit(const fs::path project_folder, const bool exist);
+	~spirit();
+}
+/*
+ * Types for basic information blocks.
+ */
+class link
+{
+private:
+	QString name;
+	QUrl addr;
+public:
+	link(const QString& name, const QUrl& addr):
+	  name(name)
+	, addr(addr){};
+	link(const std::ifstream f, const std::streampos offset);
+	~link();
+}
 
 class command
 {
 private:
-	string name;
-	string com;
+	QString name;
+	QString com;
 public:
-	command(const string& name, const string& com): name(name), com(com) {}
+	command(const QString& name, const QString& com): name(name), com(com) {};
+	command(const std::ifstream f, const std::streampos offset);
+	~command();
 }
 /*
-	Container for groups of objects
-*/
+ * Main information container for most data widjects. Stores objects grouped in user defined unions.
+ * Constructed during widjet openning.
+ * Destructed during widject closing.
+ */
 template<typename Content>
 class group_table
 {
 	using value_type = Content;
 private:
-	group<Content>* table = nullptr;
-	uint32_t sz = 0;
+	list<group<Content>> table;
 	spirit project;
 public:
 	explicit content_table(spirit project);
-	content_table();
-	group<Content>& operator[](string& group_name);
-	group<Content>& operator[](int i);
-	const group<Content>& operator[](int i) const;
- 	int size();
-	void add_group(string& group_name);
-	void remove_group(string& group_name);
-	void save();
+ 	int size() const;
+	void push_back(QString& group_name);
+	void remove(QString& group_name);
+	void move(QString& old_pos, QString& new_pos);
+	list<group<Content>>& list() const;
+	void save() const;
 	~content_table();
 }
-
-template<typename Content>
-Content* begin(group_table<Content>& x);
-
-template<typename Content>
-Content* end(group_table<Content>& x);
-
 /*
-	Container for arbitrary content (links, commands, required software and etc)
-*/
+ * Logic container for objects united in user defined group.
+ * Constructed with parent table constructor.
+ * Destructed with parent destructor.
+ */
 template<typename Content>
 class group
 {
 	using value_type = Content;
 private:
-	Content* arr = nullptr;
-	uint32_t sz = 0;
+	list<Content> data;
+	QString name;
 public:
-	string name;
-	group(const string& name): name(name){}; 
-	group(const FILE* src, const size_t offset);
-	void add(Content elem);
-	void remove(const string& name);
-	int size();
-	Content& operator[](int i);
-	const Content& operator[](int i) const;
+	group(const QString& name): name(name){}; 
+	group(const fstream src, const size_t offset);
+	QString& name();
+	void push_back(Content elem);
+	void remove(const QString& name);
+	void move(QString& old_pos, QString& new_pos);
+	list<Content>& list() const;
+	int size() const;
 	~group();
 }
-
-template<typename Content>
-Content* begin(group<Content>& x);
-
-template<typename Content>
-Content* end(group<Content>& x);
+/*
+ * Interface for file synchonizing with Google Drive.
+ * Constructed
+ * Destructed
+ */
+class sync_files
+{
+private:
+	std::vector<fs::path> syncFiles;
+public:
+	sync_files(fs::path f);
+	sync_files(const fs::path f, const std::vector<fs::path>& files_to_sync);
+	std::vector<fs::path>& list();
+	void add(const fs::path f);
+	void add(const std::vector<fs::path>& farr);
+	void remove(const fs::path f);
+	void remove(const std::vector<fs::path>& f);
+}
